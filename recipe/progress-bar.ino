b18d09af5474f9dcee280238decc6d690ce6c7c4
Keshav Saharia <keshav@keshavsaharia.com>
2014-06-25 14:07:06 -0700

    Create progress-bar.ino

---
+++ recipe/progress-bar.ino
@@ -0,0 +1,7 @@
+int width = 60;
+client.println("<div class=\"progress\">");
+  client.print("<div class=\"progress-bar\" style=\"width:");
+  client.print(width);
+  client.println("%\">");
+  client.println("</div>");
+client.println("</div>");
